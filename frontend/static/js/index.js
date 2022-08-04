ae56e768a09f616538a6aef229084931d4539bf3
ZGFuZHk100 <lorenzobarabuglini@gmail.com>
2022-08-04 11:24:54 +0200

    Done

--- frontend/static/js/index.js
+++ frontend/static/js/index.js
@@ -1,1 +1,60 @@
-console.log('js is running');
+import Dashboard from "./views/Dashboard.js";
+
+const navigateTo = url => {
+    history.pushState(null, '', url);
+    router();
+}
+
+const router = async () => {
+    const routes = [
+        {
+            path: '/',
+            view: Dashboard
+        },
+
+        {
+            path: '/posts',
+            view: () => console.log('Viewing Posts')
+        },
+
+        {
+            path: '/settings',
+            view: () => console.log('Viewing Settings')
+        }
+    ];
+
+    //Test routes for match
+    const potentialMatch = routes.map(route => {
+        return {
+            route: route,
+            isMatch: location.pathname === route.path
+        };
+    });
+
+    let match = potentialMatch.find(potentialMatch => potentialMatch.isMatch);
+
+    if(!match) {
+        match = {
+            route: routes[0],
+            isMatch: true
+        }
+    };
+
+    const view = new match.route.view();
+
+    document.querySelector('#app').innerHTML = await view.getHtml();
+};
+
+window.addEventListener('popstate', router);
+
+document.addEventListener('DOMContentLoaded', () => {
+
+    document.body.addEventListener('click', e => {
+        if(e.target.matches('.no-refresh')) {
+            e.preventDefault();
+            navigateTo(e.target.href);
+        }
+    });
+
+    router();
+});
